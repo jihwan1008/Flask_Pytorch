d32825daa1eb1b7f58e56359bcd9dad74b42b741
baejihwan <jihan1008@gist.ac.kr>
2021-01-23 23:00:31 +0900

    initial_commit

--- app.py
+++ app.py
@@ -1,6 +1,65 @@
-from flask import Flask
+import io
+import json
+import torch.nn.functional as F
+from torchvision import models
+import torchvision.transforms as transforms
+from PIL import Image
+import torch.nn as nn
+import torch
+from flask import Flask, jsonify
+import os
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+net = models.resnet34()
+num_ftrs = net.fc.in_features
+net.fc = nn.Linear(num_ftrs, 2)
+device = torch.device('cpu')
+checkpoint = torch.load('best.pth', map_location=device)
+net.load_state_dict(checkpoint['net'])
+net.eval()
+
+def transform_image(image_bytes):
+    my_transforms = transforms.Compose([transforms.Resize((224,224)),
+                                        #transforms.CenterCrop(224),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize(
+                                            [0.5, 0.5, 0.5],
+                                            [0.5, 0.5, 0.5])])
+    image = Image.open(io.BytesIO(image_bytes))
+
+    return my_transforms(image).unsqueeze(0)
+
+def get_prediction(image_bytes):
+
+    idx = str()
+    prob = float()
+    tensor = transform_image(image_bytes=image_bytes)
+    outputs = model.forward(tensor)
+    softmax = F.softmax(outputs)
+    _, predicted = outputs.max(1)
+    
+    if predicted.numpy()[0] == 1:
+        idx = 'Gingivitis'
+        prob = softmax.numpy()[0][1]
+    else:
+        idx = 'Normal'
+        prob = softmax.numpy()[0][1]
+
+    return idx, prob
+
+@app.route('/favicon.ico') 
+def favicon():     
+    return send_from_directory(os.path.join(app.root_path, 'static'),                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        file = request.files['file']
+        img_bytes = file.read()
+        diagnosis, probability = get_prediction(image_bytes=img_bytes)
+
+        return jsonify({'Diagnosis': diagnosis, 'Probability': probability})
+
+if __name__ == '__main__':
+    app.run()
